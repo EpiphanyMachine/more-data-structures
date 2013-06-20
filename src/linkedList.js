d61a42e638a71f43ec4f31e23195ff99ee854b58
EpiphanyMachine <epiphanymachine@gmail.com>
2013-06-20 13:07:02 -0700

    add and remove started

--- src/linkedList.js
+++ src/linkedList.js
@@ -1,13 +1,28 @@
-// Note: don't use an array to do this.
 var makeLinkedList = function(){
   var newLinkedList = {};
-  newLinkedList.head = null;
-  newLinkedList.tail = null;
+  // newLinkedList.head;
+  // newLinkedList.tail;
 
-  newLinkedList.addToTail = function(){
+  newLinkedList.addToTail = function(value){
+    if (value === undefined) {return;}
+    if (this.tail === undefined) {
+      this.tail = makeNode(value);
+      this.head = this.tail;
+    } else {
+      this.tail.next = makeNode(value);
+      this.tail = this.tail.next;
+    }
   };
 
   newLinkedList.removeHead = function(){
+    if (this.head === undefined) {
+      return; // if no head return undefined
+    }
+    var tempval = this.head.value;
+    //debugger;
+    if (this.head.next) {this.head = this.head.next;}
+    else {delete this.tail; delete this.head;}
+    return tempval;
   };
 
   newLinkedList.contains = function(){
@@ -26,3 +41,52 @@ var makeNode = function(value){
 
   return newNode;
 };
+
+
+
+
+// var newLinkedList = {};
+// newLinkedList.head = null;
+// newLinkedList.tail = null;
+// newLinkedList.newId = 0; //initial value
+// newLinkedList.newNodeName = function(){return JSON.stringify('node' + this.newId);};
+
+// newLinkedList.addToTail = function(value){
+//   debugger;
+//   if(this.tail === null) {
+//     // create a head (which is also the tail because the list contains 1 item)
+//     this.+newNodeName() = this.makeNode(value, null);
+//     // set as head and tail by id
+//     this.tail = this.newNodeName();
+//     this.head = this.newNodeName();
+//     // make new unique id
+//     this.newId ++;
+//   } else {
+//     // append to the current tail, set current tail as next
+//     this.newNodeName() = this.makeNode(value, this.tail);
+//     // set node as new tail
+//     this.tail = this.newNodeName();
+//     // make new unique id
+//     this.newId ++;
+//   }
+// };
+
+// newLinkedList.removeHead = function(){
+// };
+
+// newLinkedList.contains = function(){
+// };
+
+// newLinkedList.makeNode = function(value, next){
+//   var newNode = {
+//     'value': value,
+//     'next': next
+//     };
+//   return newNode;
+// };
+
+// newLinkedList.removeNextNode = function(){
+//   //isnt this the same as removeHead ???
+// };
+
+// return newLinkedList;
