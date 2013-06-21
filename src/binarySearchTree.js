5b012d65b1515d9c8b69ab36e689cbdcded50ddd
EpiphanyMachine <epiphanymachine@gmail.com>
2013-06-21 14:41:59 -0700

    depthFirstLog method was broken, rewrote and working

--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -1,63 +1,30 @@
 var BinarySearchTree = function(val){
   this.value = val;
-  this.left = null;
-  this.right = null;
-  this.parent = null;
 };
 
 BinarySearchTree.prototype.insert = function(val){
   if (this.value === val) {return;}
-  if (val < this.value && this.left.value === null){this.left = new BinarySearchTree(val); this.left.parent = this;}
-  if (val < this.value && this.left.value !== null){this.left.insert(val);}
-  if (val > this.value && this.right.value === null){this.right = new BinarySearchTree(val); this.right.parent = this;}
-  if (val > this.value && this.right.value !== null){this.right.insert(val);}
+  if (val < this.value && this.left === undefined) {this.left = new BinarySearchTree(val); this.left.parent = this;}
+  if (val < this.value && this.left !== undefined) {this.left.insert(val);}
+  if (val > this.value && this.right === undefined) {this.right = new BinarySearchTree(val); this.right.parent = this;}
+  if (val > this.value && this.right !== undefined) {this.right.insert(val);}
 };
 
 BinarySearchTree.prototype.contains = function(val){
-  return this.value === val || (val < this.value ? this.left.contains(val): this.right.contains(val));
+  if (this.value === val) {return true;}
+  if (val < this.value && this.left === undefined) {return false;}
+  if (val < this.value && this.left !== undefined) {return this.left.contains(val);}
+  if (val > this.value && this.right === undefined) {return false;}
+  if (val > this.value && this.right !== undefined) {return this.right.contains(val);}
 };
 
 BinarySearchTree.prototype.depthFirstLog = function(func){
   // calls func on each value in a tree depth first
   func.call(this.value);
-  _.invoke([this.left, this.right], 'depthFirstLog', func);
+  this.left && this.left.depthFirstLog(func);
+  this.right && this.right.depthFirstLog(func);
 };
 
 BinarySearchTree.prototype.breadthFirstLog = function(func){
   //
 };
-
-
-// **********************************************
-// Regular tree code
-// **********************************************
-
-
-// var treeMethods = {};
-// treeMethods.addChild = function(val){
-//   this.children[this.children.length] = makeTree(val);
-//   this.children[this.children.length - 1].parent = this;
-//   return this.children[this.children.length - 1];
-// };
-
-// treeMethods.contains = function(val){
-//   // returns true if and node equals val
-//   return this.value === val || _.any(_.invoke(this.children, 'contains', val));
-// };
-
-// treeMethods.traverse = function(func){
-//   // calls func on each node in a tree
-//   func.call(this);
-//   _.invoke(this.children, 'traverse', func);
-// };
-
-// treeMethods.removeFromParent = function(){
-//   // find and remove link to child in parent
-//   var newTree = this; // this changes in the each function so use a temp var
-//   _.each(this.parent.children, function(child, index, children){
-//     if(newTree === child) {children.splice(index, 1);}
-//   });
-//   // remove link to parent and return new tree
-//   this.parent = null;
-//   return newTree;
-// };
