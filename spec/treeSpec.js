77dcdcc12d38f204c5a7973694d180d485eab747
EpiphanyMachine <epiphanymachine@gmail.com>
2013-06-20 19:53:28 -0700

    rewritten to make all children trees also The code was written to make nodes, not trees for children this is now fixed to add a new tree for each child.

--- spec/treeSpec.js
+++ spec/treeSpec.js
@@ -2,13 +2,33 @@ describe("tree", function() {
   var tree;
 
   beforeEach(function() {
-    tree = makeTree();
+    tree = makeTree('top');
   });
 
   it("should have methods named 'addChild' and 'contains', and a property named 'value'", function() {
     expect(tree.addChild).toEqual(jasmine.any(Function));
     expect(tree.contains).toEqual(jasmine.any(Function));
     expect('value' in tree).toBe(true);
+  });
+
+  it("should add the first node to the top", function() {
+    expect(tree.value).toEqual('top');
+  });
+
+  it("should add newChild nodes as children", function() {
+    expect(tree.addChild('high1')).toEqual(tree.children[0]);
+    expect(tree.addChild('high2')).toEqual(tree.children[1]);
+    expect(tree.children[0].addChild('mid1')).toEqual(tree.children[0].children[0]);
+    expect(tree.children[0].addChild('mid2')).toEqual(tree.children[0].children[1]);
+  });
+
+  it("should have contains return true if a value is contained in the tree", function() {
+    tree.addChild('high1');
+    tree.addChild('high2');
+    tree.children[0].addChild('mid1');
+    tree.children[0].addChild('mid2');
+    expect(tree.contains('mid2')).toEqual(true);
+    expect(tree.contains('sdsf')).toEqual(false);
   });
 
   // Add more tests here to test the functionality of tree.
--- src/tree.js
+++ src/tree.js
@@ -1,26 +1,18 @@
 var makeTree = function(val){
-  var newTree = {};
-  newTree.value = val;
-  newTree.children = [];
+  var newTree = {
+    value: val,
+    children: []
+  };
   _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
-treeMethods.addChild = function(val, node){
-  node = node || this;
-  node.children[node.children.length] = this.makeNode(val);
-  return node.children[node.children.length - 1];
+treeMethods.addChild = function(val){
+  this.children[this.children.length] = makeTree(val);
+  return this.children[this.children.length - 1];
 };
 
-treeMethods.contains = function(val, node){
-  node = node || this;
-  return _.contains(node, val) || _.some(node.children, _.contains(val));
-};
-
-treeMethods.makeNode = function(value){
-  return {
-  'value': value,
-  'children': []
-  };
+treeMethods.contains = function(val){
+  return this.value === val || _.any(_.invoke(this.children, 'contains', val));
 };
