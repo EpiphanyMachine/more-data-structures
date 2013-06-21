3b28f8aa97af44d58bbb518c579647a27b5322d0
Hack Reactor Students <github@hackreactor.com>
2013-06-21 16:54:34 -0700

    Fixed _storage.set to set array of key, value

--- src/hashTable.js
+++ src/hashTable.js
@@ -1,5 +1,5 @@
 var HashTable = function(){
-  this._limit = 8;
+  this._limit = 4;
 
   // Use a limited array to store inserted elements.
   // It'll keep you from using too much space. Usage:
@@ -10,15 +10,54 @@ var HashTable = function(){
   this._storage = makeLimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(){
+HashTable.prototype.insert = function(key, val){
 };
 
 HashTable.prototype.retrieve = function(){
+
+};
+HashTable.prototype.remove = function(){
 };
 
-HashTable.prototype.remove = function(){
+// returns an integer hash between 0 and max for a string
+var hash = function(str, max){
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = (hash<<5) + hash + str.charCodeAt(i);
+    hash = hash & hash; // Convert to 32bit integer
+    hash = Math.abs(hash);
+  }
+  return hash % max;
 };
 
 // NOTE: For this code to work, you will NEED the code from hashTableHelpers.js
 // Start by loading those files up and playing with the functions it provides.
 // You don't need to understand how they work, only their interface is important to you
+
+// This class represents an array with limited functionality and a maximum size.
+// It will ensure that you don't accidentally try to use up too much space.
+//
+// Usage:
+//   limitedArray.set(3, 'hi');
+//   limitedArray.get(3); // returns 'hi'
+
+var makeLimitedArray = function(limit){
+  var storage = [];
+
+  var limitedArray = {};
+  limitedArray.get = function(index){
+    checkLimit(index);
+    return storage[index];
+  };
+  limitedArray.set = function(index, key, val){
+    checkLimit(index);
+    storage[index].push([key, value]);
+  };
+
+  var checkLimit = function(index){
+    if(typeof index !== 'number'){ throw new Error('setter requires a numeric index for its first argument'); }
+    if(limit <= index){ throw new Error('Error trying to access an over-the-limit index'); }
+  };
+
+  return limitedArray;
+};
