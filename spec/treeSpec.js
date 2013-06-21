8bd854210b824411520a07677928963ca8102d59
EpiphanyMachine <epiphanymachine@gmail.com>
2013-06-21 10:58:04 -0700

    removeFromParent added with associated tests returns the node called as a new tree with no parent link removes the child link from the parent.children index

--- spec/treeSpec.js
+++ spec/treeSpec.js
@@ -22,6 +22,13 @@ describe("tree", function() {
     expect(tree.children[0].addChild('mid2')).toEqual(tree.children[0].children[1]);
   });
 
+  it("should link to a parent for each child node", function() {
+    expect(tree.addChild('high1').parent).toEqual(tree);
+    expect(tree.addChild('high2').parent).toEqual(tree);
+    expect(tree.children[0].addChild('mid1').parent).toEqual(tree.children[0]);
+    expect(tree.children[0].addChild('mid2').parent).toEqual(tree.children[0]);
+  });
+
   it("should have contains return true if a value is contained in the tree", function() {
     tree.addChild('high1');
     tree.addChild('high2');
@@ -29,6 +36,26 @@ describe("tree", function() {
     tree.children[0].addChild('mid2');
     expect(tree.contains('mid2')).toEqual(true);
     expect(tree.contains('sdsf')).toEqual(false);
+  });
+
+  it("should remove a child from the children index on removeFromParent", function() {
+    tree.addChild('high1');
+    tree.addChild('high2');
+    tree.children[0].addChild('mid1');
+    tree.children[0].addChild('mid2');
+    tree.children[0].removeFromParent();
+    expect(tree.contains('high1')).toEqual(false);
+    expect(tree.contains('mid2')).toEqual(false);
+    expect(tree.children[0].value).toEqual('high2');
+  });
+
+  it("should not link a removed child to its old parent", function() {
+    tree.addChild('high1');
+    tree.addChild('high2');
+    tree.children[0].addChild('mid1');
+    tree.children[0].addChild('mid2');
+    subtree = tree.children[0].removeFromParent();
+    expect(subtree.parent).toEqual(null);
   });
 
   it("should traverse to each node in the tree", function() {
--- src/tree.js
+++ src/tree.js
@@ -1,7 +1,8 @@
 var makeTree = function(val){
   var newTree = {
     value: val,
-    children: []
+    children: [],
+    parent: null
   };
   _.extend(newTree, treeMethods);
   return newTree;
@@ -10,14 +11,39 @@ var makeTree = function(val){
 var treeMethods = {};
 treeMethods.addChild = function(val){
   this.children[this.children.length] = makeTree(val);
+  this.children[this.children.length - 1].parent = this;
   return this.children[this.children.length - 1];
 };
 
 treeMethods.contains = function(val){
+  // returns true if and node equals val
   return this.value === val || _.any(_.invoke(this.children, 'contains', val));
 };
 
 treeMethods.traverse = function(func){
+  // calls func on each node in a tree
   func.call(this);
   _.invoke(this.children, 'traverse', func);
 };
+
+treeMethods.removeFromParent = function(){
+  // find and remove link to child in parent
+  var newTree = this; // this changes in the each function so use a temp var
+  _.each(this.parent.children, function(child, index, children){
+    if(newTree === child) {children.splice(index, 1);}
+  });
+  // remove link to parent and return new tree
+  this.parent = null;
+  return newTree;
+};
+
+//****************************************************************
+// If I were to rewrite this from scratch I would use a doubly
+// linkedList for the children.  This not the children array to
+// be shuffled after something is removed from its parent.
+// You already have the link to the item being removed (this)
+// so it would be efficient.
+//
+// Because of a time constraint I will splice the children.
+//
+//****************************************************************
