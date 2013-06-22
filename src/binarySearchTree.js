cbb1ad63ff28f06ee588443c8be0f9fa296bfd55
EpiphanyMachine <epiphanymachine@gmail.com>
2013-06-22 16:38:22 -0700

    breadthFirstLog written

--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -1,21 +1,22 @@
 var BinarySearchTree = function(val){
   this.value = val;
+  this.depth = 0;
 };
 
 BinarySearchTree.prototype.insert = function(val){
   if (this.value === val) {return;}
   if (val < this.value && !this.left) {this.left = new BinarySearchTree(val); this.left.parent = this;}
-  if (val < this.value && this.left) {this.left.insert(val);}
+  if (val < this.value && this.left) {this.left.insert(val); this.depth++;}
   if (val > this.value && !this.right) {this.right = new BinarySearchTree(val); this.right.parent = this;}
-  if (val > this.value && this.right) {this.right.insert(val);}
+  if (val > this.value && this.right) {this.right.insert(val); this.depth++}
 };
 
 BinarySearchTree.prototype.contains = function(val){
   if (this.value === val) {return true;}
-  if (val < this.value && this.left === undefined) {return false;}
-  if (val < this.value && this.left !== undefined) {return this.left.contains(val);}
-  if (val > this.value && this.right === undefined) {return false;}
-  if (val > this.value && this.right !== undefined) {return this.right.contains(val);}
+  if (val < this.value && !this.left) {return false;}
+  if (val < this.value && this.left) {return this.left.contains(val);}
+  if (val > this.value && !this.right) {return false;}
+  if (val > this.value && this.right) {return this.right.contains(val);}
 };
 
 BinarySearchTree.prototype.depthFirstLog = function(func){
@@ -25,6 +26,15 @@ BinarySearchTree.prototype.depthFirstLog = function(func){
   this.right && this.right.depthFirstLog(func);
 };
 
-BinarySearchTree.prototype.breadthFirstLog = function(func){
-  //
+BinarySearchTree.prototype.breadthFirstLog = function(func, kids){
+  kids = kids || this;
+  _.each(kids, function(val){
+    func.call(val);
+  });
+  var gkids = [];
+  _.each(kids, function(val){
+    if (val.left) {gkids.push(val.left);}
+    if (val.right) {gkids.push(val.right);}
+  });
+  _.invoke(gkids, 'breadthFirstLog', [func, gkids]);
 };
