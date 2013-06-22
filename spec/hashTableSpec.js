1940b3f4d40a68def03f197170a21cb3c9259921
Hack Reactor Students <github@hackreactor.com>
2013-06-22 11:02:53 -0700

    specs written for index count, expand, reduce

--- spec/hashTableSpec.js
+++ spec/hashTableSpec.js
@@ -27,5 +27,100 @@ describe("hashTable", function() {
     expect(hashTable.retrieve('two')).toEqual('kjsdghfkjsdhf');
   });
 
-  // add more tests here to test the functionality of hashTable
+  it("should be able to remove a value", function() {
+    hashTable.insert('one', 'hklsjdhfkljsdhf');
+    hashTable.insert('hello', 'hjksdhfkjsdhfjkghsd');
+    hashTable.insert('two', 'kjsdghfkjsdhf');
+    hashTable.insert('sugar', 'whjklhfdskjh');
+    hashTable.insert('paper', 'kldshfkjh');
+    hashTable.insert('zero', 'kldsdfsdgshfkjh');
+    hashTable.insert('monkey', 'dshfkjh');
+    expect(hashTable.remove('paper')).toEqual('kldshfkjh');
+    expect(hashTable.remove('one')).toEqual('hklsjdhfkljsdhf');
+    expect(hashTable.remove('hello')).toEqual('hjksdhfkjsdhfjkghsd');
+    expect(hashTable.remove('zero')).toEqual('kldsdfsdgshfkjh');
+    expect(hashTable.remove('monkey')).toEqual('dshfkjh');
+    expect(hashTable.remove('sugar')).toEqual('whjklhfdskjh');
+    expect(hashTable.remove('two')).toEqual('kjsdghfkjsdhf');
+  });
+
+  it("should keep an accurate count of indicies used", function() {
+    hashTable.insert('one', 'hklsjdhfkljsdhf');
+    expect(hashTable._storageLength).toEqual(1);
+    hashTable.insert('hello', 'hjksdhfkjsdhfjkghsd');
+    expect(hashTable._storageLength).toEqual(2);
+    hashTable.insert('two', 'kjsdghfkjsdhf');
+    expect(hashTable._storageLength).toEqual(2);
+    hashTable.insert('sugar', 'whjklhfdskjh');
+    expect(hashTable._storageLength).toEqual(2);
+    hashTable.insert('paper', 'kldshfkjh');
+    expect(hashTable._storageLength).toEqual(2);
+    hashTable.insert('zero', 'kldsdfsdgshfkjh');
+    expect(hashTable._storageLength).toEqual(2);
+    hashTable.insert('monkey', 'dshfkjh');
+    expect(hashTable._storageLength).toEqual(3);
+    hashTable.remove('paper');
+    expect(hashTable._storageLength).toEqual(3);
+    expect(hashTable.remove('one')).toEqual('hklsjdhfkljsdhf');
+    expect(hashTable._storageLength).toEqual(3);
+    expect(hashTable.remove('hello')).toEqual('hjksdhfkjsdhfjkghsd');
+    expect(hashTable._storageLength).toEqual(3);
+    expect(hashTable.remove('zero')).toEqual('kldsdfsdgshfkjh');
+    expect(hashTable._storageLength).toEqual(2);
+    expect(hashTable.remove('monkey')).toEqual('dshfkjh');
+    expect(hashTable._storageLength).toEqual(1);
+    expect(hashTable.remove('sugar')).toEqual('whjklhfdskjh');
+    expect(hashTable._storageLength).toEqual(1);
+    expect(hashTable.remove('two')).toEqual('kjsdghfkjsdhf');
+    expect(hashTable._storageLength).toEqual(0);
+  });
+
+  it("should expand when >= 75% full", function() {
+    hashTable.insert('one', 'hklsjdhfkljsdhf');
+    expect(hashTable._limit).toEqual(4);
+    hashTable.insert('hello', 'hjksdhfkjsdhfjkghsd');
+    expect(hashTable._limit).toEqual(4);
+    hashTable.insert('two', 'kjsdghfkjsdhf');
+    expect(hashTable._limit).toEqual(4);
+    hashTable.insert('sugar', 'whjklhfdskjh');
+    expect(hashTable._limit).toEqual(4);
+    hashTable.insert('paper', 'kldshfkjh');
+    expect(hashTable._limit).toEqual(4);
+    hashTable.insert('zero', 'kldsdfsdgshfkjh');
+    expect(hashTable._limit).toEqual(4);
+    hashTable.insert('monkey', 'dshfkjh');
+    expect(hashTable._limit).toEqual(8);
+    hashTable.remove('paper');
+    expect(hashTable._limit).toEqual(8);
+    hashTable.remove('one');
+    expect(hashTable._limit).toEqual(8);
+    hashTable.remove('hello');
+    expect(hashTable._limit).toEqual(8);
+  });
+
+  it("should reduce under 25% full", function() {
+    hashTable.insert('one', 'hklsjdhfkljsdhf');
+    hashTable.insert('hello', 'hjksdhfkjsdhfjkghsd');
+    hashTable.insert('two', 'kjsdghfkjsdhf');
+    hashTable.insert('sugar', 'whjklhfdskjh');
+    hashTable.insert('paper', 'kldshfkjh');
+    hashTable.insert('zero', 'kldsdfsdgshfkjh');
+    hashTable.insert('monkey', 'dshfkjh');
+    expect(hashTable._limit).toEqual(8);
+    hashTable.remove('paper');
+    expect(hashTable._limit).toEqual(8);
+    hashTable.remove('one');
+    expect(hashTable._limit).toEqual(8);
+    hashTable.remove('hello');
+    expect(hashTable._limit).toEqual(8);
+    hashTable.remove('zero');
+    expect(hashTable._limit).toEqual(4);
+    hashTable.remove('monkey');
+    expect(hashTable._limit).toEqual(4);
+    hashTable.remove('sugar');
+    expect(hashTable._limit).toEqual(4);
+    hashTable.remove('two');
+    expect(hashTable._limit).toEqual(4);
+  });
+
 });
