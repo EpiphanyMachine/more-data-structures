43b348b83f374e63a6b61d2c181cbcc1581bdeb3
Hack Reactor Students <github@hackreactor.com>
2013-06-22 12:43:29 -0700

    added audit checks and functions to resize

--- src/hashTable.js
+++ src/hashTable.js
@@ -4,10 +4,18 @@ var HashTable = function(){
   this._storageLength = 0;
 };
 
-HashTable.prototype.insert = function(key, val){
+HashTable.prototype.insert = function(key, val, auditCheck){
+  // if noAudit was passed as argument return false to disable audit check
+  auditCheck = !(auditCheck === 'noAudit');
   var index = this.hash(key, this._limit);
   // if index is undefined then make make index 0 = [key, val] and stop
-  if (!this._storage[index]) {this._storage[index] = [[key, val]]; this._storageLength++; this._auditStorage(); return;}
+  if (!this._storage[index]) {
+    this._storage[index] = [[key, val]]; this._storageLength++;
+    // if false insert was called by the method auditStorage()
+    // which needs to complete its iterations before the check should be run again
+    if(auditCheck) {this._auditStorage();}
+    return;
+  }
   // check array for value
   var setIndex = _.map(this._storage[index], function(value, arrIndex, collection){
     if (collection[arrIndex][0] === key) {return arrIndex;}
@@ -39,9 +47,9 @@ HashTable.prototype.remove = function(key){
     if (this._storage[index][i][0] === key){setIndex = i;}
   }
   if (setIndex === false) {return;}
-  var tempval = this._storage[index].splice(setIndex,1);
+  var tempval = this._storage[index].splice(setIndex,1); // this removes the key value array
+  // this checks if the removed array was the last in the index, and runs an audit
   if (this._storage[index][0] === undefined) {this._storageLength--; this._auditStorage();}
-  //delete this._storage[index][setIndex];
   return tempval[0][1];
 };
 
@@ -57,14 +65,33 @@ HashTable.prototype.hash = function(str, max){
 };
 
 HashTable.prototype._auditStorage = function(){
-  if (this._storageLength / this._limit >= 0.75) {this._expand();}
-  if (this._storageLength / this._limit < 0.25) {this._reduce();}
+  if (this._storageLength / this._limit >= 0.75) {this._resize(2);}
+  if (this._storageLength / this._limit < 0.25 && this._storageLength !== 0) {this._resize(0.5);}
 };
 
-HashTable.prototype._expand = function(){
-  console.log('expand');
+HashTable.prototype._resize = function(multiplier){
+  this._tempStorage = this._storage;
+  this._storage = [];
+  this._storageLength = 0;
+  this._limit = this._limit * multiplier;
+  this._moveToNewStorage(this._tempStorage);
+  delete this._tempStorage;
 };
 
 HashTable.prototype._reduce = function(){
   console.log('reduce');
+};
+
+HashTable.prototype._moveToNewStorage = function(tempStore){
+  for (var i = 0; i < (this._limit / 2); i++) { // iterate through each index
+    if (tempStore[i]) { // check if undefined
+      if (tempStore[i].length > 0) { // check if empty array
+        for (var j = 0; j < tempStore[i].length; j++) { // iterate through each subArray
+          if (tempStore[i][j]) { // check if undefined
+            this.insert(tempStore[i][j][0],tempStore[i][j][1],'noAudit'); // call func(key, value)
+          }
+        }
+      }
+    }
+  }
 };
